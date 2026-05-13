a1cd901712a32305236ce72164471b3b6f72ac11
alicelucas14 <alicelucas025@gmail.com>
2026-05-13 10:15:01 +0100

    Increase Express server payload limit to 50mb

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
 
 // Note: CATEGORIES is now moved to the database for dynamic management.
